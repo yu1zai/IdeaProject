c215a10f15f1ca5961bd061e6df9a3b25ca74a63
gs <2521199534@qq.com>
2023-03-04 13:51:18 +0800

    修改后再提交

--- ideaforgit/src/org/gs/test/Hello.java
+++ ideaforgit/src/org/gs/test/Hello.java
@@ -3,5 +3,6 @@ package org.gs.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("测试idea上使用git");
+        System.out.println("我又添加了一句代码");
     }
 }
